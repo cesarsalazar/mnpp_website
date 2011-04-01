1b0f9505dc70c7d544dbacdb4a62e394c08b7dd6
César Salazar <i.am@cesarsalzar.mx>
2011-04-01 02:16:24 -0600

    Initial commit

---
+++ app.rb
@@ -0,0 +1,18 @@
+require 'rubygems'
+require 'rack-flash'
+require 'sinatra'
+require 'sinatra/content_for'
+require 'sinatra/redirect_with_flash'
+require 'sinatra/reloader' if development?
+require 'haml'
+require 'sass'
+require 'rdiscount'
+
+get '/' do
+  haml :index
+end
+
+get '/stylesheets/*' do
+  content_type 'text/css'
+  sass '../styles/'.concat(params[:splat].join.chomp('.css')).to_sym
+end
